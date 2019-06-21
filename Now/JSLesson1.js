46989f891ca1e459aa9d36da88bcf45f62f4ffac
Aleksei Nalisnik <mkumum12@mail.ru>
2019-06-22 00:01:49 +0300

    My first understandable commit for continue studying

---
+++ Now/JSLesson1.js
@@ -0,0 +1,37 @@
+'_use strict';
+
+/*var number = 5;
+var string = "Hello";
+var symb = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+console.log(4/0);
+console.log("string"*9);
+
+
+let something;
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+let arr = ['plum.png', 'orange.jpg'];
+let answer = prompt("Are u 18?", "yes");
+console.log(typeof(null));
+*/
+
+
+
+//alert, config, prompt
+
+
+    let isChecked = true,
+        isClosed = false;
+
+    console.log(!isChecked || isClosed);
