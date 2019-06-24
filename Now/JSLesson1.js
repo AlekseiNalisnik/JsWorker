0f01906e5bce5c604db744094d83db7d856bea02
Alex <mkumum12@mail.ru>
2019-06-24 12:02:40 +0300

    3rd step of my training project(FUNCTIONS)

--- Now/JSLesson1.js
+++ Now/JSLesson1.js
@@ -51,72 +51,37 @@ for(let i = 1; i < 5; i++) {
     }
     console.log(i);
 }
-
 */
 
-let money = +prompt("Ваш бюджет на месяц", ''),
-    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-let appData = { 
-    budget: money,
-    expenses: {},
-    optionalExpenses: {},
-    income: [], 
-    timeData: time,
-    savings: false 
-};
+console.log(calc(10,15));
 
-for(let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце!"),
-        b = prompt("Во сколько обойдётся?");
-    if ( (typeof(a) === "string") && (typeof(a) != null)  && (typeof(b) != null) 
-    && (a != '') && (b != '') && (a.length < 50) )  {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else {
-        i--;
-    }    
+function calc(a,b) {
+    return (a + b);
 }
 
-/*
-let i = 0;
-while (i < 2) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце!"),
-    b = prompt("Во сколько обойдётся?");
-    i++;
-    if ( (typeof(a) === "string") && (typeof(a) != null)  && (typeof(b) != null) 
-    && (a != '') && (b != '') && (a.length < 50) )  {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else {
-        i--;
-    }    
+// let calcul = function(a,b) {
+//     return (a + b);
+// }
+
+let calcul = (a,b) => a + b;
+
+console.log(calcul(10,15));
+
+function retVar() {
+    let num = 50;
+    return num;
 }
 
-do {
-    let a = prompt("Введите обязательную статью расходов в этом месяце!"),
-    b = prompt("Во сколько обойдётся?");
-    i++;
-    if ( (typeof(a) === "string") && (typeof(a) != null)  && (typeof(b) != null) 
-    && (a != '') && (b != '') && (a.length < 50) )  {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else {
-        i--;
-    }  
-} while (i < 2);
-*/
+let anotherNum = retVar();
+console.log(anotherNum);
 
-appData.moneyPerDay = appData.budget / 30;
+let str = "test";
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
 
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка");
-} else if ( (appData.moneyPerDay) > 100 && (appData.moneyPerDay < 2000) ) {
-    console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("Высокий уровень достатка");
-} else {
-    console.log("Произошла ошибка");    
-}
+let twelwe = "12.2px";
+//console.log(Math.round(twelwe));
+console.log(parseInt(twelwe));
+console.log(parseFloat(twelwe));
