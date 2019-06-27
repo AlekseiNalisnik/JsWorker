634be4130919172069f0ffcd1e0f9c6370698c38
Alex <mkumum12@mail.ru>
2019-06-27 16:38:44 +0300

    Fourt step!

--- Now/JSLesson1.js
+++ Now/JSLesson1.js
@@ -53,7 +53,7 @@ for(let i = 1; i < 5; i++) {
 }
 */
 
-
+/*
 console.log(calc(10,15));
 
 function calc(a,b) {
@@ -84,4 +84,113 @@ console.log(str.toLowerCase());
 let twelwe = "12.2px";
 //console.log(Math.round(twelwe));
 console.log(parseInt(twelwe));
-console.log(parseFloat(twelwe));
+console.log(parseFloat(twelwe));*/
+
+//CALLBACK FUNCTIONS
+/*
+function first() {
+    setTimeout( function() { 
+    console.log(1);
+    }, 500 );
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log("Я учу " + lang);
+    callback();
+}
+
+function done() {
+    console.log("Я прошёл 3-й урок!");
+}
+
+learnJS("JavaScript", done);*/
+
+//OBJECTS
+/*
+let options = {
+    width: 1024,
+    height: 1024,
+    name: "Test"
+};
+
+console.log(options.name);
+
+options.bool = false;
+options.colors = {
+    border: "Black",
+    bg: "red"
+};
+
+delete options.bool;
+
+console.log(options);
+
+for(let key in options) {
+    console.log('Свойство ' + key + " имеет значение " + options[key]);
+}
+console.log(Object.keys(options).length);
+
+let arr = ["first", 2, 3, "four", 5];
+
+arr.pop(); // удаление из конца массива
+arr.push("5"); // добавление в конец массива
+arr.shift() // удаление с начала массива
+arr.unshift("5"); // добавление в начало массива
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+arr.forEach(function(item, i, mass) {
+    console.log(i + ": " + item + "(массив: " + mass + ')');
+});
+
+let mass = [1,3,4,6,7];
+
+for(let key of mass) {
+    console.log(key);
+}
+
+console.log(arr);
+*/
+
+// let ans = prompt("", ""),
+//     array = [];
+    
+// array = ans.split(','); // передаёт в массив значения, которые введены в промпт через запятую
+// console.log(array);
+
+// let arr = ["aaa", "bbb", "ccc", "111"],
+//     i = arr.join(", "); // склеивает элементы массива в одну строку
+// console.log(i);
+
+let arr = ["aaa", "bbb", "ccc", "111"],
+    i = arr.sort(compareNum); // сортирует элементы массива по алфавиту
+
+function compareNum(a,b) {
+    return a - b;
+} // callback функция для сортировки по возрастанию чисел, записанных в массив
+
+console.log(i);
+
+let soldier = {
+    health: 400,
+    armor: 100
+}
+
+let John = {
+    health: 100
+}
+
+John.__proto__ = soldier;
+
+console.log(John);
+console.log(John.armor);
+
